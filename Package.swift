29369e79c9c95968c345dc80b920e6b1a35473b9
marcusseixascaf <150820175+marcusseixascaf@users.noreply.github.com>
2025-01-29 16:23:36 -0300

    update package dependencies

--- Package.swift
+++ Package.swift
@@ -1,15 +1,13 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "CafSDK",
-    platforms: [
-        .iOS(.v13)
-    ],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "CafSDK",
-            targets: ["CafSDKCore"]
+            targets: ["Core"]
         ),
         .library(
             name: "DocumentDetector",
@@ -17,15 +15,15 @@ let package = Package(
         ),
         .library(
             name: "CafFaceLiveness",
-            targets: ["CafFaceLivenessCore"]
-        ),
-        .library(
-            name: "IproovProvider",
-            targets: ["IproovProvider"]
+            targets: ["CafFaceLiveness"]
         ),
         .library(
             name: "FaceTec2DProvider",
             targets: ["FaceTec2DProvider"]
+        ),
+        .library(
+            name: "IproovProvider",
+            targets: ["IproovProvider"]
         )
     ],
     dependencies: [
@@ -44,55 +42,75 @@ let package = Package(
         .package(
             url: "https://github.com/combateafraude/CafSolutions.git",
             .exact("1.0.2")
-        )
+        ),
     ],
     targets: [
+        .binaryTarget(
+            name: "DocumentDetectorBinary",
+            path: "Frameworks/DocumentDetector.xcframework"
+        ),
+        .binaryTarget(
+            name: "CafFaceLivenessCoreBinary",
+            path: "Frameworks/CafFaceLiveness.xcframework"
+        ),
+        .binaryTarget(
+            name: "IproovProviderBinary",
+            path: "Frameworks/IproovProvider.xcframework"
+        ),
+        .binaryTarget(
+            name: "FaceTec2DProviderBinary",
+            path: "Frameworks/FaceTec2DProvider.xcframework"
+        ),
+
         .target(
-            name: "CafSDKCore",
-            dependencies: [
-                "DocumentDetectorCore",
-                "CafFaceLivenessCore"
-            ],
+            name: "Core",
+            dependencies: ["DocumentDetector", "CafFaceLiveness"],
             path: "Sources/Core"
         ),
+        
         .target(
-            name: "DocumentDetectorCore",
+            name: "DocumentDetector",
             dependencies: [
-                "DocumentDetector",
+                "DocumentDetectorBinary",
                 .product(name: "TensorFlowLiteC", package: "TensorFlowLiteC"),
                 .product(name: "CafSolutions", package: "CafSolutions")
             ],
             path: "Sources/DocumentDetector"
         ),
-        .binaryTarget(
-            name: "DocumentDetector",
-            path: "Frameworks/DocumentDetector.xcframework"
-        ),
+        
         .target(
             name: "CafFaceLivenessCore",
             dependencies: [
-                "IproovProvideCore",
-                "FaceTec2DProvider",
+                "CafFaceLivenessCoreBinary",
                 .product(name: "FingerprintPro", package: "fingerprintjs-pro-ios"),
                 .product(name: "CafSolutions", package: "CafSolutions")
             ],
-            path: "Sources/CafFaceLiveness"
+            path: "Sources/CafFaceLivenessCore"
         ),
+        
         .target(
-            name: "IproovProvideCore",
+            name: "IproovProvider",
             dependencies: [
-                "DocumentDetector",
-                .product(name: "iProov", package: "ios")
+                "IproovProviderBinary",
+                .product(name: "iProov", package: "ios"),
+                "CafFaceLivenessCore"
             ],
             path: "Sources/IproovProvider"
         ),
-        .binaryTarget(
-            name: "IproovProvider",
-            path: "Frameworks/IproovProvider.xcframework"
-        ),
-        .binaryTarget(
+        
+        .target(
             name: "FaceTec2DProvider",
-            path: "Frameworks/FaceTec2DProvider.xcframework"
+            dependencies: [
+                "FaceTec2DProviderBinary",
+                "CafFaceLivenessCore"
+            ],
+            path: "Sources/FaceTec2DProvider"
+        ),
+        
+        .target(
+            name: "CafFaceLiveness",
+            dependencies: ["CafFaceLivenessCore", "IproovProvider", "FaceTec2DProvider"],
+            path: "Sources/CafFaceLiveness"
         )
     ]
 )
